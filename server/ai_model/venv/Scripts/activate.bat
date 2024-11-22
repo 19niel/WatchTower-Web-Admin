0a70695081778c1d1b5361487a3270652663f429
Motskii <xyoo549@gmail.com>
2024-11-22 19:10:41 +0800

    may AI na ulit

--- server/ai_model/venv/Scripts/activate.bat
+++ server/ai_model/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Admin\Documents\GitHub\WatchTower-Web-Admin\server\ai_model\venv
+set VIRTUAL_ENV=C:\Users\LENOVO\Documents\Capstone\WatchTower-Web-Admin\server\ai_model\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
